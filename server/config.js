db30a2b028cd52e10545030c4390dfd824caf3fd
Jitendra Singh <jsingh@weycorp.com>
2018-08-22 02:24:31 +0545

    add mlab crdtls

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 
 // modify this!!!
 module.exports = {
-    db: "mongodb://localhost:27017",
+    db: "mongodb://sjeeten:mataheroho123@ds127362.mlab.com:27362",
     port: 3001,
     secret: "mysecret"
 }
